78477830b31fee88226bf3a023bd9a178714ba88
harishankarpv <harishankarpv99@gmail.com>
2021-09-28 13:15:01 +0530

    Updates

---
+++ precache-manifest.66e92de61f1f522c14a0a3bcfc300634.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe53917c84c72a5840b79aa03734c3ce",
+    "url": "/neoPortfolioDemo/index.html"
+  },
+  {
+    "revision": "621aeb49104c766e4f4a",
+    "url": "/neoPortfolioDemo/static/css/main.7a6ae8a0.chunk.css"
+  },
+  {
+    "revision": "e4a7525f14eaad0f059f",
+    "url": "/neoPortfolioDemo/static/js/2.e4fa4635.chunk.js"
+  },
+  {
+    "revision": "32eaebd0b06f4bb60d5e9e5d0e7cd13e",
+    "url": "/neoPortfolioDemo/static/js/2.e4fa4635.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621aeb49104c766e4f4a",
+    "url": "/neoPortfolioDemo/static/js/main.062dbe2a.chunk.js"
+  },
+  {
+    "revision": "d8834b6f98a68c015577",
+    "url": "/neoPortfolioDemo/static/js/runtime-main.2ed9be92.js"
+  },
+  {
+    "revision": "53c383668d49bd98ce55f80003534b3c",
+    "url": "/neoPortfolioDemo/static/media/100.53c38366.jpg"
+  },
+  {
+    "revision": "8f1df092adab46fa893e132c254792c4",
+    "url": "/neoPortfolioDemo/static/media/100.8f1df092.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/neoPortfolioDemo/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/neoPortfolioDemo/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/neoPortfolioDemo/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/neoPortfolioDemo/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/neoPortfolioDemo/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/neoPortfolioDemo/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/neoPortfolioDemo/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/neoPortfolioDemo/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "5868034752086839e51be3984d374080",
+    "url": "/neoPortfolioDemo/static/media/QuantumComp.58680347.jpeg"
+  },
+  {
+    "revision": "a1051a228140c97862ba9a7f321c92b6",
+    "url": "/neoPortfolioDemo/static/media/buld.a1051a22.jpg"
+  },
+  {
+    "revision": "b3685be8bafcd5fe4786b39cba69a561",
+    "url": "/neoPortfolioDemo/static/media/codeInLogo.b3685be8.png"
+  },
+  {
+    "revision": "7a9b2eb087350abacd2587dd578226c7",
+    "url": "/neoPortfolioDemo/static/media/deeplearning_ai_logo.7a9b2eb0.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/neoPortfolioDemo/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/neoPortfolioDemo/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/neoPortfolioDemo/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/neoPortfolioDemo/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/neoPortfolioDemo/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/neoPortfolioDemo/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/neoPortfolioDemo/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/neoPortfolioDemo/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/neoPortfolioDemo/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/neoPortfolioDemo/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/neoPortfolioDemo/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/neoPortfolioDemo/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/neoPortfolioDemo/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/neoPortfolioDemo/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/neoPortfolioDemo/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/neoPortfolioDemo/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "80980b5b4e2c6e980a92810b8e51f511",
+    "url": "/neoPortfolioDemo/static/media/harry.80980b5b.jpeg"
+  },
+  {
+    "revision": "ccf886614a9c68f209d3e95c2626dd99",
+    "url": "/neoPortfolioDemo/static/media/ino.ccf88661.png"
+  },
+  {
+    "revision": "29152cf756a1facead5393c32e373371",
+    "url": "/neoPortfolioDemo/static/media/intel_logo.29152cf7.jpg"
+  },
+  {
+    "revision": "d9ff570b6fb7530ab04342093299b787",
+    "url": "/neoPortfolioDemo/static/media/portfolio.d9ff570b.png"
+  },
+  {
+    "revision": "e56f163e80842bcfa28db7076b28861a",
+    "url": "/neoPortfolioDemo/static/media/quicko.e56f163e.jpg"
+  },
+  {
+    "revision": "62ac09c36353106943a259590cf62a9e",
+    "url": "/neoPortfolioDemo/static/media/sal.62ac09c3.png"
+  },
+  {
+    "revision": "da0e7c80b90df28dd3a590c80f3d4970",
+    "url": "/neoPortfolioDemo/static/media/stanford_logo.da0e7c80.png"
+  },
+  {
+    "revision": "c54faf1740c35847ac1c016c81beb35b",
+    "url": "/neoPortfolioDemo/static/media/vjt.c54faf17.png"
+  },
+  {
+    "revision": "02fdae4350ca45cabdc9c40cc11efc2a",
+    "url": "/neoPortfolioDemo/static/media/wrighter.02fdae43.jpg"
+  }
+]);
